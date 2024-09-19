8af331165ef1025f25a82546a7a154fc290af3b9
Matteo Albinola <malbinola@sonicle.com>
2024-09-19 11:25:31 +0200

    Fix grid grouping style

--- src/main/client/Service.js
+++ src/main/client/Service.js
@@ -475,7 +475,7 @@ Ext.define('Sonicle.webtop.contacts.Service', {
 								'<span class="{[this.getGroupCls(values)]}">{[this.getGroupLabel(values)]}</span>',
 								{
 									getGroupCls: function(values) {
-										return 'wt-form-body-title' + ((values.groupField.indexOf('letter') !== -1) ? ' wtcon-group-letter' : '');
+										return ((values.groupField.indexOf('letter') !== -1) ? ' wtcon-group-letter' : '');
 									},
 									getGroupLabel: function(values) {
 										var label;
