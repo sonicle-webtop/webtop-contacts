67eb0c5f47a3b734418c07b8356b65fb35f78572
malbinola <malbinola@sonicle.com>
2025-12-02 16:57:56 +0100

    Use new wt-tool-scroll-inset CSS class

--- src/main/client/Service.js
+++ src/main/client/Service.js
@@ -354,7 +354,7 @@ Ext.define('Sonicle.webtop.contacts.Service', {
 					},
 					border: false,
 					scrollable: { trackPosition: 'v' },
-					cls: 'wt-tool-tree wtcon-tool-tree',
+					cls: 'wt-tool-tree wt-tool-scroll-inset wtcon-tool-tree',
 					bodyCls: 'wt-tool-bg',
 					width: '100%',
 					flex: 1
